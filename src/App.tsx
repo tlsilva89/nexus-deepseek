c68e53533371f6d23a844b1789e2911ac53e7e82
Thiago Luciano <tlsilva89@icloud.com>
2025-04-13 16:25:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400 animate-bounce">
+        Tailwind + React + TypeScript!
+      </h1>
+    </div>
+  )
+}
